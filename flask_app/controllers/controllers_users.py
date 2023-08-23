26c5b4ed8aa810ef6e5e409117726411841c0bac
Brandon Colangelo <brandoncancode@gmail.com>
2023-08-23 11:45:58 -0400

    Added video for some projects,  Javascript, style.

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -6,3 +6,13 @@ from flask import render_template, redirect
 @app.get('/')
 def index():
     return render_template('homepage.html')
+
+# Route for rendering the view Game Vault page.
+@app.get('/view_game_vault')
+def view_game_vault():
+    return render_template('game_vault.html')
+
+# Route for rendering the view Heroes and Villains page.
+@app.get('/view_heroes_and_villains')
+def view_heroes_and_villains():
+    return render_template('heroes_and_villains.html')
