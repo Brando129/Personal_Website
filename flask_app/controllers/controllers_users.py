6b8f47062cd1273275c7931ff60b1b8ad4d4f488
Brandon Colangelo <brandoncancode@gmail.com>
2023-08-29 15:51:33 -0400

    Added clock and calc projects.

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import render_template, redirect
+from flask import render_template
 
 # Get Routes
 # Route for rendering the Home Page.
@@ -7,12 +7,22 @@ from flask import render_template, redirect
 def index():
     return render_template('homepage.html')
 
-# Route for rendering the view Game Vault page.
-@app.get('/view_game_vault')
+# Route for rendering the Game Vault page.
+@app.get('/game_vault')
 def view_game_vault():
     return render_template('game_vault.html')
 
 # Route for rendering the view Heroes and Villains page.
-@app.get('/view_heroes_and_villains')
+@app.get('/heroes_and_villains')
 def view_heroes_and_villains():
     return render_template('heroes_and_villains.html')
+
+# Route for rendering the clock page.
+@app.get('/clock')
+def clock():
+    return render_template('clock.html')
+
+# Route for rendering the calculator page.
+@app.get('/calculator')
+def calculator():
+    return render_template('calculator.html')
--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -26,3 +26,87 @@ function pause_game_vault() {
 function scale(element, value) {
     element.style.transform = "scale(" + value + ")";
 }
+
+// Clock
+function getSecondsSinceStartOfDay() {
+    return new Date().getSeconds() +
+      new Date().getMinutes() * 60 +
+      new Date().getHours() * 3600;
+}
+
+function getHourHandAngle(s) {
+    s %= 43200; // seconds in 12 hours
+    return ((360 * s / 43200) + 180) % 360;
+}
+
+function getMinuteHandAngle(s) {
+    s %= 3600; // seconds in an hour
+    return ((6 * s / 60) + 180) % 360;
+}
+
+function getSecondHandAngle(s) {
+    s %= 60; // seconds in a minute
+    return ((6 * s) + 180) % 360;
+}
+
+const hour = document.getElementById("hour");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+setInterval( () => {
+    let s = getSecondsSinceStartOfDay();
+    hour.style.transform = `rotate(${getHourHandAngle(s)}deg)`;
+    minutes.style.transform = `rotate(${getMinuteHandAngle(s)}deg)`;
+    seconds.style.transform = `rotate(${getSecondHandAngle(s)}deg)`;
+}, 1000);
+// End of Clock
+
+
+// Calculator
+const display = document.getElementById("display");
+let num1 = "";
+let num2 = "";
+let op = "";
+
+
+function press(num) {
+    num1 += num;
+    display.innerHTML = num1;
+}
+
+function setOP(key) {
+    op = key;
+    num2 = num1;
+    num1 = "";
+}
+
+function clr() {
+    num1 = "";
+    num2 = "";
+    op = "";
+    display.innerHTML = "0";
+}
+
+function calculate() {
+    let a = parseFloat(num2);
+    let b = parseFloat(num1);
+    let res = 0;
+    switch(op) {
+    case "+":
+        res = a + b;
+        break;
+    case "-":
+        res = a - b;
+        break;
+    case "*":
+        res = a * b;
+        break;
+    case "/":
+        res = a / b;
+        break;
+}
+    num1 = res;
+    op = "";
+    display.innerHTML = res;
+}
+// End of Calculator
