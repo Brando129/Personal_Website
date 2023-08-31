dfb2a9cfa6a1298a6af65868d19ccdae0a84cb8f
Brandon Colangelo <brandoncancode@gmail.com>
2023-08-31 13:41:02 -0400

    Made one universal video viewing HTML page.

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import render_template
+from flask import render_template, redirect, request, session
 
 # Get Routes
 # Route for rendering the Home Page.
@@ -8,14 +8,14 @@ def index():
     return render_template('homepage.html')
 
 # Route for rendering the Game Vault page.
-@app.get('/game_vault')
-def view_game_vault():
-    return render_template('game_vault.html')
+# @app.get('/game_vault')
+# def view_game_vault():
+#     return render_template('game_vault.html')
 
 # Route for rendering the view Heroes and Villains page.
-@app.get('/heroes_and_villains')
-def view_heroes_and_villains():
-    return render_template('heroes_and_villains.html')
+# @app.get('/heroes_and_villains')
+# def view_heroes_and_villains():
+#     return render_template('heroes_and_villains.html')
 
 # Route for rendering the clock page.
 @app.get('/clock')
@@ -29,15 +29,27 @@ def calculator():
 
 # Route for rendering the The Wall page.
 @app.get('/wall')
-def wall():
-    return render_template('wall.html')
+# def wall():
+#     return render_template('wall.html')
 
 # Route for rendering the Recipes page.
-@app.get('/recipes')
-def recipes():
-    return render_template('recipes.html')
+# @app.get('/recipes')
+# def recipes():
+#     return render_template('recipes.html')
 
-# Route for rendering the Recipes page.
-@app.get('/ohana')
-def ohana():
-    return render_template('ohana.html')
+# Route for rendering Ohana Rideshares page.
+# @app.get('/ohana')
+# def ohana():
+#     return render_template('ohana.html')
+
+# Route for rendering the View Videos page.
+@app.get('/view_video')
+def view_video():
+    return render_template('view_video.html')
+
+# Post Routes
+@app.post('/click/view_video')
+def click_view_video():
+    session['url'] = request.form['file_path']
+    session['project_name'] = request.form['project_name']
+    return redirect('/view_video')
